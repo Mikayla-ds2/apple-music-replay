f99f8dddd72a9395e37441ebe5825cfd86bbe205
Mikayla Solomon <mikaylasolomon68@gmail.com>
2024-12-27 02:38:13 -0500

    changed clean file, got the token and set the auth stuff up, now looking into different metadata

--- spotify_api.py
+++ spotify_api.py
@@ -34,3 +34,38 @@ track_info = {
 }
 
 print(track_info)
+
+import pandas as pd
+play_history = pd.read_csv('clean_play_history.csv')
+
+spotify_data = []
+
+for index, row in play_history.iterrows():
+    track_name = row['Track Description']
+    artist_name = row['Artist Name']
+    params = {
+        'q': f'track:{track_name} artist:{artist_name}',
+        'type': 'track',
+        'limit': 1
+    }
+    response = requests.get(search_url, headers=headers, params=params) 
+    data = response.json()
+    
+    if data['tracks']['items']: 
+        track = data['tracks']['items'][0]
+        track_info = {
+            'Track Name': track['name'],
+            'Artist Name': track['artists'][0]['name'],
+            'Album Name': track['album']['name'],
+            'Release Date': track['album']['release_date'],
+            'Popularity': track['popularity'],
+            'Duration (ms)': track['duration_ms'],
+            'Spotify Track ID': track['id'],
+            'Spotify URL': track['external_urls']['spotify'],
+        }
+        spotify_data.append(track_info)
+    else:
+        print(f"No track found for {track_name} by {artist_name}")
+        
+spotify_df = pd.DataFrame(spotify_data)
+spotify_df.to_csv('spotify_metadata.csv', index=False)
